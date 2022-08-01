7cf75ae92b3a2ec82d23333fbcc49e683bb1e1af
José Vitor Do Valle Jorge <67615532+josedovalle@users.noreply.github.com>
2022-08-01 12:07:09 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center">:file_cabinet: Joga da adivinhação - Explorer</h1>
+
+## :memo: Descrição
+Primeiro projeto manipulando a DOM e integrando HTML, CSS e JavaScript!
+
+## :wrench: Tecnologias utilizadas
+* HTML5;
+* CSS3;
+* JavaScript;
---
+++ main.js
@@ -0,0 +1,42 @@
+// variáveis
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
+
+// eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+/*btnReset.addEventListener('keydown', function(e) {
+  if(e.key == 'Enter') {
+    handleResetClick()
+  }
+})*/
+
+// funções callback ou metedos
+function handleTryClick(event) {
+  event.preventDefault() // não faça o padrão
+
+  const inputNumber = document.querySelector("#inputNumber")
+
+  if(Number(inputNumber.value) == randomNumber) {
+    toggleScreen()
+
+    screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
+  }
+
+  inputNumber.value = ""
+  xAttempts++
+}
+
+function handleResetClick() {
+  toggleScreen()
+  xAttempts = 1
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
